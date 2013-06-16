037550f11b8ac11a97c754c2fe820734baf8a1f9
Jon McKenzie <jcmcken@gmail.com>
2013-06-16 05:31:04 -0400

    Add additional metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
   name='jgen',
   version=open('VERSION').read().strip(),
+  description='Generate simple JSON documents from the command line.',
+  author='Jon McKenzie',
+  license='BSD',
+  url='https://github.com/jcmcken/jgen',
   packages=find_packages(),
   entry_points={
     'console_scripts': ['jgen = jgen:main']
